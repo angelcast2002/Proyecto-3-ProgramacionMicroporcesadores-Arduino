7e93b40566ab5c6b249634757aa6a0ab037afddb
Ángel Castellanos <77862762+angelcast2002@users.noreply.github.com>
2022-11-23 10:32:52 -0600

    Final

--- SensorDePulso/SensorDePulso.ino
+++ SensorDePulso/SensorDePulso.ino
@@ -1,84 +1,71 @@
 
-/*  Getting_BPM_to_Monitor prints the BPM to the Serial Monitor, using the least lines of code and PulseSensor Library.
- *  Tutorial Webpage: https://pulsesensor.com/pages/getting-advanced
- *
---------Use This Sketch To------------------------------------------
-1) Displays user's live and changing BPM, Beats Per Minute, in Arduino's native Serial Monitor.
-2) Print: "♥  A HeartBeat Happened !" when a beat is detected, live.
-2) Learn about using a PulseSensor Library "Object".
-4) Blinks LED on PIN 13 with user's Heartbeat.
+/* Sensor de pulso
+  Proyecto 3
+  Angel Castellanos 21700
+  Mario Cristales 21631
+  Javier Ramirez 21600
+  Jose Santisteban 21153
 --------------------------------------------------------------------*/
 
-#define USE_ARDUINO_INTERRUPTS true  // Set-up low-level interrupts for most acurate BPM math.
-#include <PulseSensorPlayground.h>   // Includes the PulseSensorPlayground Library.
+#define USE_ARDUINO_INTERRUPTS true  // Se define el nivel de interrupcion de la libreria para el sensor de pulso
+#include <PulseSensorPlayground.h>   // Se incluye la libreria del sensor de pulso
 
 //  Variables
-const int photocellPin = A1;
-int val = 0;
-const int PulseWire = 0;  // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
-const int LED5 = 05;      // The on-board Arduino LED, close to PIN 13.
-int Threshold = 515;      // Determine which Signal to "count as a beat" and which to ignore.
-                          // Use the "Gettting Started Project" to fine-tune Threshold Value beyond default setting.
-                          // Otherwise leave the default "550" value.
+const int photocellPin = A1;       // Se define el pin de la fotocelda
+const int PulseWire = 0;  // Se define el pin del sensor de pulso
+const int LED5 = 05;      // Se define el pin del LED
+int Threshold = 515;      // Se define el umbral del sensor de pulso
 
-int touchInput = 3;
-int inputState;
-int lastInputState = 1;
-int dt = 100;
-int myBPM;
-int luminosidad = 0;
+int touchInput = 3;      // Se define el pin del sensor de toque
+int inputState;         // Se define la variable para el estado del sensor de toque
+int dt = 100;          // Se define el tiempo de espera para el sensor de toque
+int myBPM;            // Se define la variable para el pulso
+int luminosidad = 0; // Se define la variable para la luminosidad
 
-PulseSensorPlayground pulseSensor;  // Creates an instance of the PulseSensorPlayground object called "pulseSensor"
+PulseSensorPlayground pulseSensor;  // Se crea el objeto para el sensor de pulso
 
 
 void setup() {
 
-  Serial.begin(9600);  // For Serial Monitor
-  pinMode(photocellPin, INPUT);
-  // Configure the PulseSensor object, by assigning our variables to it.
+  Serial.begin(9600);  // Se inicia la comunicacion serial
+  pinMode(photocellPin, INPUT); // Se define el pin de la fotocelda como entrada
+
+  // Se inicia el sensor de pulso
   pulseSensor.analogInput(PulseWire);
-  pulseSensor.blinkOnPulse(LED5);  //auto-magically blink Arduino's LED with heartbeat.
+  pulseSensor.blinkOnPulse(LED5);  
   pulseSensor.setThreshold(Threshold);
   pinMode(touchInput, INPUT);
 
   delay(200);
-  // Double-check the "pulseSensor" object was created and "began" seeing a signal.
-  if (pulseSensor.begin()) {
-      //This prints one time at Arduino power-up,  or on Arduino reset.
-  }
+
 }
 
 
 
 void loop() {
-  inputState = digitalRead(touchInput);
+  inputState = digitalRead(touchInput); // Se lee el estado del sensor de toque
   delay(20);
-  if (inputState == HIGH) {
-    int valorMax = 140;
-    int valorMin = 40;
-    // "myBPM" hold this BPM value now.
-    while (inputState == HIGH) {
+  if (inputState == HIGH) { // Si el sensor de toque esta presionado
+    
+    while (inputState == HIGH) { // Una vez presionado el sensor de toque se lee el puslo hasta que se vuelva a presionar
 
-      luminosidad = analogRead(photocellPin);
-      Serial.print(luminosidad);
-      Serial.print(",");
-      myBPM = pulseSensor.getBeatsPerMinute();  // Calls function on our pulseSensor object that returns BPM as an "int".
-                                                // "myBPM" hold this BPM value now.
+      luminosidad = analogRead(photocellPin); // Se lee la luminosidad
+      Serial.print(luminosidad); // Se imprime la luminosidad
+      Serial.print(","); // Se imprime una coma para separar los datos
+      myBPM = pulseSensor.getBeatsPerMinute();  // Se lee el pulso y se guarda en la variable myBPM
+      
 
-      // "myBPM" hold this BPM value now.
-
-
-      Serial.print(myBPM);  // Print the value inside of myBPM.
-      Serial.println("a");
+      Serial.print(myBPM);  // Se imprime el pulso
+      Serial.println("a"); // Se imprime una a para separar los datos
       delay(500);
 
 
-      inputState = digitalRead(touchInput);
+      inputState = digitalRead(touchInput); // Se lee el estado del sensor de toque
       delay(200);
-      if (inputState == LOW) {
+      if (inputState == LOW) { //Si el sensor no se toca no sucede nada, por esto se debe seguir dentor del loop
         inputState = 1;
       } else {
-        inputState = 0;
+        inputState = 0; // Si el sensor se toca se sale del loop
         break;
       }
     }
